eded8c92b0320f46c6321ed5d6e4037656c7653a
Brian <108324541+FueledByOatmilk@users.noreply.github.com>
2024-10-31 17:59:12 -0700

    Stage 1
    This camera controller should always be centered on the Vessel. There are no additional fields to be serialized and usable in the inspector. Done
    
    Your controller should draw a five-by-unit cross in the center of the screen when draw_camera_logic is true. Done

--- Obscura/scripts/camera_controllers/push_box.gd
+++ Obscura/scripts/camera_controllers/push_box.gd
@@ -12,33 +12,14 @@ func _ready() -> void:
 	
 
 func _process(delta: float) -> void:
-	if !current:
+	if !target:
 		return
 	
 	if draw_camera_logic:
 		draw_logic()
 	
-	var tpos = target.global_position
-	var cpos = global_position
+	global_position = target.global_position
 	
-	#boundary checks
-	#left
-	var diff_between_left_edges = (tpos.x - target.WIDTH / 2.0) - (cpos.x - box_width / 2.0)
-	if diff_between_left_edges < 0:
-		global_position.x += diff_between_left_edges
-	#right
-	var diff_between_right_edges = (tpos.x + target.WIDTH / 2.0) - (cpos.x + box_width / 2.0)
-	if diff_between_right_edges > 0:
-		global_position.x += diff_between_right_edges
-	#top
-	var diff_between_top_edges = (tpos.z - target.HEIGHT / 2.0) - (cpos.z - box_height / 2.0)
-	if diff_between_top_edges < 0:
-		global_position.z += diff_between_top_edges
-	#bottom
-	var diff_between_bottom_edges = (tpos.z + target.HEIGHT / 2.0) - (cpos.z + box_height / 2.0)
-	if diff_between_bottom_edges > 0:
-		global_position.z += diff_between_bottom_edges
-		
 	super(delta)
 
 
@@ -46,36 +27,32 @@ func draw_logic() -> void:
 	var mesh_instance := MeshInstance3D.new()
 	var immediate_mesh := ImmediateMesh.new()
 	var material := ORMMaterial3D.new()
-	
+
 	mesh_instance.mesh = immediate_mesh
 	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
-	
-	var left:float = -box_width / 2
-	var right:float = box_width / 2
-	var top:float = -box_height / 2
-	var bottom:float = box_height / 2
-	
+
+	# Da cross dimensions (5 units total)
+	var cross_size = 2.5
+
 	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
-	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
-	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
-	
-	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
-	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
-	
-	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
-	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
-	
-	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
-	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
+
+	# Drawing of the horizontal line
+	immediate_mesh.surface_add_vertex(Vector3(-cross_size, 0, 0))
+	immediate_mesh.surface_add_vertex(Vector3(cross_size, 0, 0))
+
+	# Drawing of the vertical line
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, -cross_size))
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, cross_size))
+
 	immediate_mesh.surface_end()
 
 	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	material.albedo_color = Color.BLACK
-	
+	material.albedo_color = Color.WHITE
+
 	add_child(mesh_instance)
 	mesh_instance.global_transform = Transform3D.IDENTITY
 	mesh_instance.global_position = Vector3(global_position.x, target.global_position.y, global_position.z)
-	
-	#mesh is freed after one update of _process
+
+	# Free the mesh after one frame update
 	await get_tree().process_frame
 	mesh_instance.queue_free()

f4f167b389aeb035ecc8e2c3db12f23ca0e97989
Brian <108324541+FueledByOatmilk@users.noreply.github.com>
2024-10-31 20:02:43 -0700

    Create and pushbox2
    Created additional nodes and completed stage 2

--- Obscura/scripts/camera_controllers/push_box2.gd
+++ Obscura/scripts/camera_controllers/push_box2.gd
@@ -1,33 +1,49 @@
+class_name PushBox2
 extends CameraControllerBase
 
-@export var top_left: Vector2          # Top-left corner of the frame border box
-@export var bottom_right: Vector2      # Bottom-right corner of the frame border box
-@export var autoscroll_speed: Vector3  # Speed of auto-scroll in the x and z axes
+@export var top_left: Vector2         
+@export var bottom_right: Vector2     
+@export var autoscroll_speed: Vector3 
+@export var box_width: float = 10.0
+@export var box_height: float = 10.0
+
+func _ready() -> void:
+	super()
+	position = target.position
 
 func _process(delta: float) -> void:
-	if !target:
+	if !current:
 		return
 
-	# Auto-scroll the camera frame on the x-z plane
+	# Autoscroll frame on the x and z axes
 	global_position.x += autoscroll_speed.x * delta
 	global_position.z += autoscroll_speed.z * delta
 
-	# Define the frame edges in world space
-	var frame_left = global_position.x + top_left.x
-	var frame_right = global_position.x + bottom_right.x
-	var frame_top = global_position.z + top_left.y
-	var frame_bottom = global_position.z + bottom_right.y
+	var tpos = target.global_position
+	var cpos = global_position
 
-	# Push the player forward if touching the left edge of the frame
-	if target.global_position.x < frame_left:
+	# Frame boundary edges
+	var frame_left = cpos.x + top_left.x
+	var frame_right = cpos.x + bottom_right.x
+	var frame_top = cpos.z + top_left.y
+	var frame_bottom = cpos.z + bottom_right.y
+
+	# Boundary checks for left
+	if tpos.x < frame_left:
 		target.global_position.x = frame_left
+	if tpos.x > frame_right:
+		target.global_position.x = frame_right
+	if tpos.z > frame_top:
+		target.global_position.z = frame_top
+	if tpos.z < frame_bottom:
+		target.global_position.z = frame_bottom
 
-	# Draw the frame border if draw_camera_logic is enabled
 	if draw_camera_logic:
-		draw_frame_border(frame_left, frame_right, frame_top, frame_bottom)
+		draw_logic()
 
-# Function to draw the frame border box in the x-z plane
-func draw_frame_border(left: float, right: float, top: float, bottom: float) -> void:
+	super(delta)
+
+func draw_logic() -> void:
 	var mesh_instance := MeshInstance3D.new()
 	var immediate_mesh := ImmediateMesh.new()
 	var material := ORMMaterial3D.new()
@@ -35,25 +51,30 @@ func draw_frame_border(left: float, right: float, top: float, bottom: float) -> 
 	mesh_instance.mesh = immediate_mesh
 	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
 
+	var left: float = top_left.x
+	var right: float = bottom_right.x
+	var top: float = top_left.y
+	var bottom: float = bottom_right.y
+
 	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
 
-	# Draw the border of the frame box in 3D space
-	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
-	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
-	
+	# Draw the frame border in 3D space
 	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
 	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
-	
+
 	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
 	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
-	
+
 	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
 	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
+
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
 
 	immediate_mesh.surface_end()
 
 	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	material.albedo_color = Color.WHITE
+	material.albedo_color = Color.BLACK
 
 	add_child(mesh_instance)
 	mesh_instance.global_transform = Transform3D.IDENTITY
--- Obscura/scripts/camera_controllers/push_box5.gd
+++ Obscura/scripts/camera_controllers/push_box5.gd
@@ -1,4 +1,4 @@
-extends Node
+extends CameraControllerBase
 
 
 # Called when the node enters the scene tree for the first time.
